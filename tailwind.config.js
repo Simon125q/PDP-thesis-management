2766fd9d6e38b68f5824e1b2e776d1f878e23dae
Simon125q <sz.omiecinski@gmail.com>
2024-11-04 13:38:50 +0100

    added login view

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: ["./**/*.html", "./**/*.templ", "./**/*.go"],
   safelist: [],
   theme: {
-    colors: {
-      "tul-dark-red": "#8b0002",
-      "tul-medium-red": "#a43537",
-      "tul-light-red": "#c47979",
-      "tul-dark-gray": "#1d1d1d",
+    extend: {
+      colors: {
+        "tul-dark-red": "#8b0002",
+        "tul-medium-red": "#832941",
+        "tul-light-red": "#c47979",
+        "tul-dark-gray": "#1d1d1d",
+      },
     },
   },
 };
