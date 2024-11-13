57725f0efa44752683779a11ea70a261df5310a6
pdp24-4 <pdp24-4@sr.dmcs.pl>
2024-11-13 14:24:00 +0000

    runlinux test

--- runLinux.sh
+++ runLinux.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-# to run the app on linux run ./runLinux.sh
+# Function to kill any running process
+kill_running_process() {
+    # Killing the Go application if it exists
+    pid=$(ps aux | grep 'tmp/main' | grep -v 'grep' | awk '{print $2}')
+    if [ -n "$pid" ]; then
+        echo "Killing existing Go application with PID: $pid"
+        kill -9 $pid
+    fi
+    
+    # Killing any running templ generate if exists
+    pid=$(ps aux | grep 'templ generate' | grep -v 'grep' | awk '{print $2}')
+    if [ -n "$pid" ]; then
+        echo "Killing existing templ generate process with PID: $pid"
+        kill -9 $pid
+    fi
+}
 
 # Run 'make css' in the background
-nohup make css &
+make css &
 
-# Run 'air' in the background
-nohup air &
+# Stop any existing templ generate process
+kill_running_process
 
-# Run 'templ generate --watch --proxy=http://localhost:3000' in the background
-nohup templ generate --watch --proxy=http://localhost:3000 &
+# Run 'templ generate' (the main process)
+templ generate &
 
+# Build Go application
+/usr/bin/go/go/bin/go build -tags=dev -o ./tmp/main .
+
+# Start the Go application in the background with nohup
+nohup ./tmp/main &
+
+# Ensure Jenkins job doesn't wait for the background processes
+echo "App is running independently."
+
+# End of script, Jenkins will finish, but app will keep running
 
