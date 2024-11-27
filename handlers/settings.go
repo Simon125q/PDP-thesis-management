e0b6453f59f0def540f88a81b29227f3a343b59b
pdp24-4 <pdp24-4@sr.dmcs.pl>
2024-11-27 20:38:15 +0100

    started settings, minor spelling tweaks

--- handlers/settings.go
+++ handlers/settings.go
@@ -1,10 +1,17 @@
 package handlers
 
 import (
+	"log/slog"
 	"net/http"
+	"thesis-management-app/pkgs/server"
 	"thesis-management-app/views/settings"
 )
 
 func HandleSettings(w http.ResponseWriter, r *http.Request) error {
-	return Render(w, r, settings.Index())
+	empl_data, err := server.MyS.DB.AllUniversityEmployee()
+	slog.Info("quere", "q", r.URL.Query())
+	if err != nil {
+		return err
+	}
+	return Render(w, r, settings.Index(empl_data))
 }
