64cc9503bda0c55132a972b1d150a8bb7b913b73
Simon125q <sz.omiecinski@gmail.com>
2024-12-16 23:56:39 +0100

    added new validators

--- pkgs/validators/Employee.go
+++
@@ -1,7 +0,0 @@
-package validators
-
-import "thesis-management-app/types"
-
-func ValidateEmployee(t types.UniversityEmployee) (types.UniversityEmployeeErrors, bool) {
-	return types.UniversityEmployeeErrors{}, true //if validation is needed, idk if it is tbh
-}
